71136c062ee84c71d8c2f4616bc675eaed8bc2f6
SYLVIE MOLINATTO s318952 <s318952@studenti.polito.it>
2023-09-20 12:35:26 +0200

    adding material

---
+++ 01TXYOV - WEB APPLICATION I/Laboratori/Lab11/server/db.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** DB access module **/
+
+const sqlite = require('sqlite3');
+
+// open the database
+exports.db = new sqlite.Database('films.db', (err) => {
+  if (err) throw err;
+});
+
+
